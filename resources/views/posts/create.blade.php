cb43b183a17ea0fe8db9aa7f1f3dd8ace08d7bfc
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-04-25 08:13:45 -0700

    Form Validation 101

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="col-sm-8 blog-main">
-        <h1>Create a post</h1>
+<div class="col-sm-8 blog-main">
+    <h1>Create a post</h1>
+    <hr>
+    <form method="POST" action="/posts">
 
-<hr>
-<form method="POST" action="/posts">
+        {{ csrf_field() }}
 
-{{ csrf_field() }}
+        <div class="form-group">
+            <label for="title">Title:</label>
+            <input type="text" class="form-control" id="title" name="title">
+        </div>
+        <div class="form-group">
+            <label for="body">Body</label>
+            <textarea id="body" name="body" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <button type="submit" class="btn btn-default">Publish</button>
+        </div>
 
-  <div class="form-group">
-    <label for="title">Title:</label>
-    <input type="text" class="form-control" id="title" name="title">
-  </div>
-  <div class="form-group">
-    <label for="body">Body</label>
-    <textarea id="body" name="body" class="form-control"></textarea>
-  </div>
+        @include('layouts.errors')
 
-
-  <button type="submit" class="btn btn-default">Publish</button>
-</form>
-    </div>
-
+    </form>
+</div>
 @endsection
