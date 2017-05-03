792021260f2b455d15c0e8b52426c9b63d181e02
Makoto Ishihara <makoto.ishihara.jp@gmail.com>
2017-05-03 01:18:05 -0700

    Testing 101

--- app/Post.php
+++ app/Post.php
@@ -23,6 +23,6 @@ class Post extends Model
         }
     }
     public static function archives(){
-    	return static:: selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')->groupBy('year', 'month')->orderByRaw('min(created_at) desc')->get()->toArray();
+    	return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')->groupBy('year', 'month')->orderByRaw('min(created_at) desc')->get()->toArray();
     }
 }
